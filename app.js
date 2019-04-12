7b37b1461113395d6347a7071e0afec1615229e2
Литвинов <litvinovMS@studio-tg.ru>
2019-04-12 17:49:00 +0300

    Add config initialization.

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-const logger = require('./logger');
+const _connectConfig = () => {
+  const config = require('./config');
+  const path = require('path');
+  const fs = require('fs');
+  const parseArgs = require('minimist');
+
+  const args = parseArgs(process.argv.slice(2));
+
+  const { config: configPath } = args;
+
+  if (configPath) {
+    const isAbsolute = configPath[0] === '/';
+    const parsedPath = isAbsolute ? configPath : path.join(__dirname, '../', configPath);
+    if (fs.existsSync(parsedPath)) {
+      conf.file({ file: parsedPath });
+    } else {
+      const errorText = `${Date.now()}: error - Configuration file \'${parsedPath}\' could not be located`;
+      console.log(`${errorText}, no logging path specified!`);
+      process.exit(1);
+    }
+  }
+};
 
 const _init = () => {
+  const logger = require('./logger');
+
   logger.log('build', 'Initializing application.');
+
   const started = Date.now();
   try {
     const createControlPanel = require('./application/control/createControlServer');
@@ -14,4 +38,10 @@ const _init = () => {
   }
 };
 
-_init()
+try {
+  _connectConfig();
+} catch (err) {
+  console.log(`Error connecting configuration: ${err.message}`);
+}
+
+_init();
--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-module.exports = {
+const conf = require('nconf');
+
+conf.defaults({
   entryPoint: 'api.riotgames.com/lol',
   APIKey: 'RGAPI-3f3ca381-7bbf-4c37-821d-20a2d2f2c26c',
   region: 'RU',
   defaultSeason: 13,
   defaultQueue: 420,
-};
+});
+
+module.exports = conf;
